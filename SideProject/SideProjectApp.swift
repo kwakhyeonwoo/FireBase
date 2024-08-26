040d8ab8afe366803edd309b1be8ee03a9bf291c
곽현우 <polla723@naver.com>
2024-08-27 00:36:29 +0900

    Initial Commit

---
+++ SideProject/SideProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideProjectApp.swift
+//  SideProject
+//
+//  Created by 곽현우 on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SideProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
